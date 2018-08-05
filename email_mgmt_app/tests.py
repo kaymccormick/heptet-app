40c7108e288c5ca1b2773da70a1f48ec8546a928
Kay McCormick <kay@kaymccormick.com>
2018-08-05 16:46:04 -0700

    attempt to fix

--- email_mgmt_app/tests.py
+++ email_mgmt_app/tests.py
@@ -12,13 +12,14 @@ class BaseTest(unittest.TestCase):
     def setUp(self):
         self.config = testing.setUp(settings={
             'sqlalchemy.url': 'sqlite:///:memory:'
+
         })
         self.config.include('.models')
         settings = self.config.get_settings()
 
-        from email_mgmt_app.entity.model import get_tm_session
-        from email_mgmt_app.entity.model import get_session_factory
-        from email_mgmt_app.entity.model import get_engine
+        from email_mgmt_app.models import get_tm_session
+        from email_mgmt_app.models import get_session_factory
+        from email_mgmt_app.models import get_engine
 
         self.engine = get_engine(settings)
         session_factory = get_session_factory(self.engine)
