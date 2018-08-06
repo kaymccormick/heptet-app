4f6ecf6a07992d44cfa84a3f4ac3eb142cf21de1
Kay McCormick <kay@kaymccormick.com>
2018-08-06 11:27:22 -0700

    add deparate dev and prod npm run scripts, comment

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,6 @@
- const path = require('path');
+/* Webpack configuration - common */
+
+const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin')
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,3 +1,5 @@
+/* Webpack configuration - dev */
+
 const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
