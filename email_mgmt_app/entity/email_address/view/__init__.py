35ba95da8ccacb6dd977f49179c765a2902722c1
Kay McCormick <kay@kaymccormick.com>
2018-08-15 15:16:41 -0700

    successfully add an email address

--- email_mgmt_app/entity/email_address/view/__init__.py
+++ email_mgmt_app/entity/email_address/view/__init__.py
@@ -1,4 +1,4 @@
-from email_mgmt_app.entity.model.email_mgmt import Host
+from email_mgmt_app.entity.model.email_mgmt import Host, EmailAddress
 from pyramid.request import Request
 from pyramid.view import view_config
 from email_mgmt_app.views.default import munge_dict
@@ -7,9 +7,17 @@ from email_mgmt_app.views.default import munge_dict
 @view_config(route_name='email_form', renderer='templates/email_address/email_form.jinja2')
 def email_form_view(request: Request) -> dict:
     d = {'hosts': request.dbsession.query(Host).all()}
-    return munge_dict(request, {})
+    return munge_dict(request, d)
 
 @view_config(route_name='email_create', renderer='templates/email_address/email_create.jinja2')
 def email_create_view(request: Request) -> dict:
-    return munge_dict(request, {})
+    d = {}
+    # create e-mail entry
+    email = EmailAddress()
+    email.name = request.POST['local_part']
+    email.host_id = request.POST['host_id']
+    request.dbsession.add(email)
+    request.dbsession.flush()
+    d['email'] = email
+    return munge_dict(request, d)
 
