1b7c26ce6c438a8da804e58fdd0d278d57ad8ad0
Kay McCormick <kay@kaymccormick.com>
2018-09-10 19:44:29 -0700

    saving this stuff cuz i am gonna delete it

---
+++ src/entry_point/organization_role_person_form.js
@@ -0,0 +1,20 @@
+
+import jQuery from "jquery";
+window.$ = window.jQuery = jQuery;
+import 'popper.js';
+import 'bootstrap';
+import React, { Component} from 'react';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+jQuery(document).ready(function() {
+    let o = {};
+
+});
+
+
+
+
+
+
+
+
