e76032cd8946c658f7ab3e0f68bfc6ad15d9715f
Kay McCormick <kay@kaymccormick.com>
2018-09-11 03:25:01 -0700

    fixed another test, tho by hardcoding the EV

--- tests/config/test_resourceManager.py
+++ tests/config/test_resourceManager.py
@@ -1,17 +1,16 @@
-from email_mgmt_app import RootResource
+from email_mgmt_app import RootResource, get_root
 import logging
 
 logger = logging.getLogger(__name__)
 
-def test_rm_add_action(config_fixture, resource_manager):
+
+def test_rm_add_action(config_fixture, resource_manager, app_request):
     logger.warning("%s", resource_manager)
     config_fixture.add_resource_manager(resource_manager)
-    #config_fixture.action(None, resource_manager.add_action)
+    # config_fixture.action(None, resource_manager.add_action)
     config_fixture.commit()
-    root = RootResource()
-    assert len(root.items()) == 1
-
-
-
-
-
+    root = get_root(app_request)
+    for k, v in root.items():
+        logger.warning("%s = %s", k, v)
+    #logger.warning("%s", root.items())
+    assert 16 == len(root.items())
