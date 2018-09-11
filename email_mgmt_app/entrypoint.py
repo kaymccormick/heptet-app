8cc9ec52a02ad67c574c0f7e8d3704b9c3bfbab5
Kay McCormick <kay@kaymccormick.com>
2018-09-10 22:27:54 -0700

    Fix for broken ep gen

--- email_mgmt_app/entrypoint.py
+++ email_mgmt_app/entrypoint.py
@@ -124,7 +124,8 @@ js=%r, view_kwargs=%r, mapper_wrapper=%r, template_name=%r, template=%r, output_
         self._template = template
 
     def init_generator(self, registry, root_namespace, template_env):
-        gctx = GeneratorContext(self.mapper_wrapper.get_one_mapper_info(), template_env, TemplateVars(),
+        w = self.mapper_wrapper and self.mapper_wrapper.get_one_mapper_info() or None
+        gctx = GeneratorContext(w, template_env, TemplateVars(),
                                 form_context_factory=FormContext, root_namespace=root_namespace)
         generator = registry.getAdapter(gctx, IEntryPointGenerator)
         self.generator = generator
