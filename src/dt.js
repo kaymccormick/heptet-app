0b081de6f0034ac8026b46ef1ec71f82f1892e91
Kay McCormick <kay@kaymccormick.com>
2018-10-01 01:22:08 -0700

    remove src

--- src/dt.js
+++
@@ -1,8 +0,0 @@
-import $ from 'jquery'
-import 'datatables.net'
-
-function engage() {
-    $(document).ready(function() {
-    $('#domain-table').DataTable();
-} );
-}
--- src/index.prod.js
+++
@@ -1,3 +0,0 @@
-import 'bootstrap';
-import 'script-loader!../node_modules/jquery/dist/jquery.slim.min';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
